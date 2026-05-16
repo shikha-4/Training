9cb4d37146284608cd428cef9e304b27a0051513
shikha-4 <shikhatk1@gmail.com>
2026-05-16 19:43:33 +0530

    Add files via upload

---
+++ PRIME.py
@@ -0,0 +1,21 @@
+n=int(input("number: "))
+if n<=1:
+    print("neither prime")
+else:
+    for i in range(2,n):
+        if n%i==0:
+            print("comp")
+            break
+    else:
+        print("prime")
+            
+
+  
+        
+            
+        
+
+        
+    
+
+        
